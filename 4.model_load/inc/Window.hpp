bde2b613df614a18d0a35092dfe9e9dcac2512d8
yeonseong <yeonseong.dev@gmail.com>
2022-12-29 17:25:16 +0900

    ENV: apple env

--- 4.model_load/inc/Window.hpp
+++ 4.model_load/inc/Window.hpp
@@ -39,6 +39,10 @@ Window::Window(int w, int h, const std::string &n)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // 최소버전: 그냥 glfw 버전
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 프로파일 버전: 코어
 
+	#ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	#endif
+
 	_window_ptr = glfwCreateWindow(_width, _height, _name.c_str(), 0, 0);
 	if (_window_ptr == NULL) {
 		throw std::runtime_error("[Window] : fail to create window");
